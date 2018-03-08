d7ef5cee091accf332cb180b7231ae9cd28559c6
Quim <quim@airmail.cc>
2018-03-08 21:10:54 +0100

    Atoi
    https://leetcode.com/problems/string-to-integer-atoi/description/

---
+++ strings/atoi/atoi.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int atoi(std::string str) {
+    int res = 0;
+    bool negative = false;
+
+    bool whitespaces = true;
+    for (char &c : str) {
+        if ((c == ' ' || c == '\t') && whitespaces) {
+            continue;
+        }
+
+        whitespaces = false;
+        if (c == '+') continue;
+        else if (c == '-') negative = true;
+        else if (c > '0' && c < '9') {
+            res = (10 * res) + (c - '0');
+        } else break;
+    }
+
+    return res;
+}
+
+int main() {
+    std::string str = "   1234";
+    std::cout << atoi(str) << std::endl;
+}
